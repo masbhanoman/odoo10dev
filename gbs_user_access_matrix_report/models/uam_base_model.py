a42b08fe0b7c7c4caeace9c7c3da703930471c87
masbhanoman <masbhanoman@gmail.com>
2021-11-07 16:41:16 +0600

    removed comment

--- gbs_user_access_matrix_report/models/uam_base_model.py
+++ gbs_user_access_matrix_report/models/uam_base_model.py
@@ -3,5 +3,4 @@ from odoo import models, fields, api, _
 class UamBaseModel(models.Model):
     _name = 'uam.base.model'
     _description = 'uam base model'
-    #_rec_name = 'patient_name'
     name = fields.Char(string='Name', required=True)
